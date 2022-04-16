159daa5043ab8b8cb04158c12802fe3f295dafec
Marcelo Zilio <marcelo.zilio@hotmail.com>
2022-04-16 13:26:04 -0300

    initial commit

---
+++ src/main/java/com/zilio/labs/wikipediareader/WikipediaReaderApplication.java
@@ -0,0 +1,13 @@
+package com.zilio.labs.wikipediareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaReaderApplication.class, args);
+	}
+
+}
